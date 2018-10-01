928716435aa1b07a3eea1b3aad411930770a9994
jsville <b@beauxarts.co.uk>
2018-10-01 19:22:25 +0300

    Add files via upload

---
+++ preview/resources/story.js
@@ -0,0 +1,1216 @@
+var story = {
+"docName": "bitmal-03",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"pages": [
+{
+"image": "22_transaction_history.png",
+"image2x": "22_transaction_history@2x.png",
+"width": 1400,
+"height": 1579,
+"title": "22 Transaction history",
+'type': 'regular',
+"links": [
+]}
+,{
+"image": "21_my_projects.png",
+"image2x": "21_my_projects@2x.png",
+"width": 1400,
+"height": 1695,
+"title": "21 My Projects",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+]}
+,{
+"image": "20_explore_user_menu.png",
+"image2x": "20_explore_user_menu@2x.png",
+"width": 1400,
+"height": 1695,
+"title": "20 Explore User Menu",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1075,
+    130,
+    1262,
+    162
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    1075,
+    130,
+    1262,
+    162
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    1075,
+    162,
+    1289,
+    194
+   ],
+   "page": 0
+  }
+,{
+  "rect": [
+    1075,
+    162,
+    1289,
+    194
+   ],
+   "page": 0
+  }
+,{
+  "rect": [
+    1075,
+    194,
+    1289,
+    226
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    1075,
+    194,
+    1289,
+    226
+   ],
+   "page": 1
+  }
+]}
+,{
+"image": "19_explore_logged_in.png",
+"image2x": "19_explore_logged_in@2x.png",
+"width": 1400,
+"height": 1695,
+"title": "19 Explore Logged in",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1134,
+    48,
+    1300,
+    93
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    1134,
+    48,
+    1300,
+    93
+   ],
+   "page": 2
+  }
+]}
+,{
+"image": "18_sign_up_(volunteer)2.png",
+"image2x": "18_sign_up_(volunteer)2@2x.png",
+"width": 1400,
+"height": 1579,
+"title": "18 Sign up (Volunteer)2",
+'type': 'regular',
+"links": [
+]}
+,{
+"image": "17_sign_up_(volunteer).png",
+"image2x": "17_sign_up_(volunteer)@2x.png",
+"width": 1400,
+"height": 1579,
+"title": "17 Sign up (Volunteer)",
+'type': 'regular',
+"links": [
+]}
+,{
+"image": "16_sign_up.png",
+"image2x": "16_sign_up@2x.png",
+"width": 1400,
+"height": 1579,
+"title": "16 Sign up",
+'type': 'regular',
+"links": [
+]}
+,{
+"image": "15_sign_in.png",
+"image2x": "15_sign_in@2x.png",
+"width": 1400,
+"height": 1579,
+"title": "15 Sign in",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    406,
+    474,
+    473,
+    504
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    406,
+    474,
+    473,
+    504
+   ],
+   "page": 3
+  }
+]}
+,{
+"image": "14_submit_project_error.png",
+"image2x": "14_submit_project_error@2x.png",
+"width": 1400,
+"height": 1579,
+"title": "14 Submit Project Error",
+'type': 'regular',
+"links": [
+]}
+,{
+"image": "13_submit_project_with_task.png",
+"image2x": "13_submit_project_with_task@2x.png",
+"width": 1400,
+"height": 2089,
+"title": "13 Submit Project with Task",
+'type': 'regular',
+"links": [
+]}
+,{
+"image": "12_submit_project_with_milestones.png",
+"image2x": "12_submit_project_with_milestones@2x.png",
+"width": 1400,
+"height": 2089,
+"title": "12 Submit Project with Milestones",
+'type': 'regular',
+"links": [
+]}
+,{
+"image": "11_submit_project.png",
+"image2x": "11_submit_project@2x.png",
+"width": 1400,
+"height": 1579,
+"title": "11 Submit Project",
+'type': 'regular',
+"links": [
+]}
+,{
+"image": "10_back_project_over_goal.png",
+"image2x": "10_back_project_over_goal@2x.png",
+"width": 1400,
+"height": 1579,
+"title": "10 Back project over goal",
+'type': 'regular',
+"links": [
+]}
+,{
+"image": "09_back_project_backing.png",
+"image2x": "09_back_project_backing@2x.png",
+"width": 1400,
+"height": 1579,
+"title": "09 Back project Backing",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    406,
+    583,
+    528,
+    613
+   ],
+   "page": 12
+  }
+,{
+  "rect": [
+    406,
+    583,
+    528,
+    613
+   ],
+   "page": 12
+  }
+]}
+,{
+"image": "08_back_project.png",
+"image2x": "08_back_project@2x.png",
+"width": 1400,
+"height": 1579,
+"title": "08 Back project",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    406,
+    499,
+    688,
+    547
+   ],
+   "page": 13
+  }
+,{
+  "rect": [
+    406,
+    499,
+    688,
+    547
+   ],
+   "page": 13
+  }
+]}
+,{
+"image": "07_project_page_with_milestones_&_update.png",
+"image2x": "07_project_page_with_milestones_&_update@2x.png",
+"width": 1400,
+"height": 2939,
+"title": "07 Project page with Milestones & Update",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    100,
+    2508,
+    235,
+    2538
+   ],
+   "page": 14
+  }
+,{
+  "rect": [
+    100,
+    2508,
+    235,
+    2538
+   ],
+   "page": 14
+  }
+,{
+  "rect": [
+    1230,
+    56,
+    1300,
+    86
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1230,
+    56,
+    1300,
+    86
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1165,
+    64,
+    1208,
+    79
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    1165,
+    64,
+    1208,
+    79
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    100,
+    64,
+    151,
+    79
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    100,
+    64,
+    151,
+    79
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    175,
+    64,
+    282,
+    94
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    175,
+    64,
+    282,
+    94
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+]}
+,{
+"image": "06_project_page.png",
+"image2x": "06_project_page@2x.png",
+"width": 1400,
+"height": 2439,
+"title": "06 Project page",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    100,
+    2044,
+    235,
+    2074
+   ],
+   "page": 14
+  }
+,{
+  "rect": [
+    100,
+    2044,
+    235,
+    2074
+   ],
+   "page": 14
+  }
+,{
+  "rect": [
+    1230,
+    56,
+    1300,
+    86
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1230,
+    56,
+    1300,
+    86
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1165,
+    64,
+    1208,
+    79
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    1165,
+    64,
+    1208,
+    79
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    100,
+    64,
+    151,
+    79
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    100,
+    64,
+    151,
+    79
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    175,
+    64,
+    282,
+    94
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    175,
+    64,
+    282,
+    94
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+,{
+  "rect": [
+    916,
+    867,
+    1300,
+    897
+   ],
+   "page": 12
+  }
+,{
+  "rect": [
+    916,
+    867,
+    1300,
+    897
+   ],
+   "page": 12
+  }
+]}
+,{
+"image": "05_explore_select_sorting.png",
+"image2x": "05_explore_select_sorting@2x.png",
+"width": 1400,
+"height": 1695,
+"title": "05 Explore Select sorting",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1230,
+    56,
+    1300,
+    86
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1230,
+    56,
+    1300,
+    86
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1165,
+    64,
+    1208,
+    79
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    1165,
+    64,
+    1208,
+    79
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    100,
+    64,
+    151,
+    79
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    100,
+    64,
+    151,
+    79
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    175,
+    64,
+    282,
+    94
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    175,
+    64,
+    282,
+    94
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+]}
+,{
+"image": "04_explore_select_city.png",
+"image2x": "04_explore_select_city@2x.png",
+"width": 1400,
+"height": 1695,
+"title": "04 Explore Select City",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    626,
+    191,
+    761,
+    227
+   ],
+   "page": 17
+  }
+,{
+  "rect": [
+    626,
+    191,
+    761,
+    227
+   ],
+   "page": 17
+  }
+,{
+  "rect": [
+    100,
+    191,
+    235,
+    227
+   ],
+   "page": 19
+  }
+,{
+  "rect": [
+    100,
+    191,
+    235,
+    227
+   ],
+   "page": 19
+  }
+,{
+  "rect": [
+    397,
+    191,
+    532,
+    227
+   ],
+   "action": "back"
+  }
+,{
+  "rect": [
+    1230,
+    56,
+    1300,
+    86
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1230,
+    56,
+    1300,
+    86
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1165,
+    64,
+    1208,
+    79
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    1165,
+    64,
+    1208,
+    79
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    100,
+    64,
+    151,
+    79
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    100,
+    64,
+    151,
+    79
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    175,
+    64,
+    282,
+    94
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    175,
+    64,
+    282,
+    94
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+]}
+,{
+"image": "03_explore_select_cause.png",
+"image2x": "03_explore_select_cause@2x.png",
+"width": 1400,
+"height": 1695,
+"title": "03 Explore Select cause",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    626,
+    191,
+    761,
+    227
+   ],
+   "page": 17
+  }
+,{
+  "rect": [
+    626,
+    191,
+    761,
+    227
+   ],
+   "page": 17
+  }
+,{
+  "rect": [
+    100,
+    191,
+    235,
+    227
+   ],
+   "action": "back"
+  }
+,{
+  "rect": [
+    397,
+    191,
+    532,
+    227
+   ],
+   "page": 18
+  }
+,{
+  "rect": [
+    397,
+    191,
+    532,
+    227
+   ],
+   "page": 18
+  }
+,{
+  "rect": [
+    1230,
+    56,
+    1300,
+    86
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1230,
+    56,
+    1300,
+    86
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1165,
+    64,
+    1208,
+    79
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    1165,
+    64,
+    1208,
+    79
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    100,
+    64,
+    151,
+    79
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    100,
+    64,
+    151,
+    79
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    175,
+    64,
+    282,
+    94
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    175,
+    64,
+    282,
+    94
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+]}
+,{
+"image": "02_explore.png",
+"image2x": "02_explore@2x.png",
+"width": 1400,
+"height": 1695,
+"title": "02 Explore",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    626,
+    191,
+    761,
+    227
+   ],
+   "page": 17
+  }
+,{
+  "rect": [
+    626,
+    191,
+    761,
+    227
+   ],
+   "page": 17
+  }
+,{
+  "rect": [
+    100,
+    191,
+    235,
+    227
+   ],
+   "page": 19
+  }
+,{
+  "rect": [
+    100,
+    191,
+    235,
+    227
+   ],
+   "page": 19
+  }
+,{
+  "rect": [
+    397,
+    191,
+    532,
+    227
+   ],
+   "page": 18
+  }
+,{
+  "rect": [
+    397,
+    191,
+    532,
+    227
+   ],
+   "page": 18
+  }
+,{
+  "rect": [
+    1230,
+    56,
+    1300,
+    86
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1230,
+    56,
+    1300,
+    86
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1165,
+    64,
+    1208,
+    79
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    1165,
+    64,
+    1208,
+    79
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    100,
+    64,
+    151,
+    79
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    100,
+    64,
+    151,
+    79
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    175,
+    64,
+    282,
+    94
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    175,
+    64,
+    282,
+    94
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+]}
+,{
+"image": "01_landing_page.png",
+"image2x": "01_landing_page@2x.png",
+"width": 1400,
+"height": 2061,
+"title": "01 Landing page",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    100,
+    652,
+    484,
+    1052
+   ],
+   "page": 16
+  }
+,{
+  "rect": [
+    100,
+    652,
+    484,
+    1052
+   ],
+   "page": 16
+  }
+,{
+  "rect": [
+    508,
+    349,
+    639,
+    379
+   ],
+   "page": 11
+  }
+,{
+  "rect": [
+    508,
+    349,
+    639,
+    379
+   ],
+   "page": 11
+  }
+,{
+  "rect": [
+    1230,
+    56,
+    1300,
+    86
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1230,
+    56,
+    1300,
+    86
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1165,
+    64,
+    1208,
+    79
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    1165,
+    64,
+    1208,
+    79
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    100,
+    64,
+    151,
+    79
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    100,
+    64,
+    151,
+    79
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    175,
+    64,
+    282,
+    94
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    175,
+    64,
+    282,
+    94
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+,{
+  "rect": [
+    625,
+    48,
+    775,
+    93
+   ],
+   "page": 21
+  }
+]}
+   ]
+,"resolutions": [2],
+"title": "Bitmal_03",
+"highlightLinks": false
+}
